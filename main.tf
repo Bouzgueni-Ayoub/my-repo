b910447d9f40148916739ecb29ea620eefa03e5a
ayoub bouzgueni <bouzgueniayoub@gmail.com>
2025-06-06 07:52:23 +0000

    g

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  force_destroy = true  # Optional: allows terraform destroy to delete non-empty buckets
+
+  tags = {
+    Name = var.bucket_name
+    Environment = "Test"
+  }
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
